1038f19540d006f12e5900137c74b16fa1a61b6f
Jonathan Adithya <jo.adithya391@gmail.com>
2021-05-23 12:08:57 +0700

    update lowest price in spreadsheet

--- main.py
+++ main.py
@@ -17,8 +17,8 @@ if sheet_data[0]['iataCode'] == '':
     data_manager.update_code()
 
 today = dt.datetime.now()
-tomorrow = today + dt.timedelta(days=1)
-six_months = today + dt.timedelta(days=6 * 30)
+tomorrow = (today + dt.timedelta(days=1)).strftime('%d/%m/%Y')
+six_months = (today + dt.timedelta(days=6 * 30)).strftime('%d/%m/%Y')
 
 account_sid = os.environ['TWILIO_ACCOUNT_SID']
 auth_token = os.environ['TWILIO_AUTH_TOKEN']
@@ -32,12 +32,14 @@ for destination in sheet_data:
         to_date=six_months
     )
 
-    if flight_data.price < destination['price'] or destination['price'] == '':
+    if flight_data is not None and (flight_data.price < destination['lowestPrice'] or destination['lowestPrice'] == 0):
         message = client.messages.create(
             body=f"Low Price Alert!\n"
-                 f"Only Rp {flight_data.price} to fly from {flight_data.from_city}-{flight_data.from_airport}"
+                 f"Only Rp {flight_data.price} to fly from {flight_data.from_city}-{flight_data.from_airport} "
                  f"to {flight_data.to_city}-{flight_data.to_airport}, "
                  f"from {flight_data.from_date} to {flight_data.return_date}.",
             from_=os.environ.get('VIRTUAL_PHONE'),
             to=os.environ.get('PHONE_NUMBER')
         )
+        destination['lowestPrice'] = flight_data.price
+data_manager.update_price()
