8d9d3ecd5c21fa9bcaa5a22683ea385ff4e8efd8
Jonathan Adithya <jo.adithya391@gmail.com>
2021-05-23 11:33:42 +0700

    create function to search available flights in a span of 6 months

--- flight_search.py
+++ flight_search.py
@@ -1,3 +1,4 @@
+from flight_data import FlightData
 from dotenv import load_dotenv
 import requests
 import os
@@ -19,5 +20,34 @@ def get_code(city):
     return code
 
 
-class FlightSearch:
-    pass
+def check_flights(from_city, to_city, from_date, to_date):
+    parameters = {
+        'fly_from': from_city,
+        'fly_to': to_city,
+        'date_from': from_date,
+        'date_to': to_date,
+        'nights_in_dst_from': 5,
+        'nights_in_dst_to': 14,
+        'flight_type': round,
+        'one_for_city': 1,
+        "max_stopovers": 2,
+        'curr': 'IDR'
+    }
+    response = requests.get(url=f'{TEQUILA_ENDPOINT}/v2/search', params=parameters, headers=headers)
+
+    try:
+        data = response.json()["data"][0]
+    except IndexError:
+        print(f'No flights found for {to_city} in a span of six months.')
+        return
+
+    return FlightData(
+        price=data['price'],
+        airline=data['route']['airline'],
+        from_city=data['route']['cityFrom'],
+        to_city=data['route']['cityTo'],
+        from_airport=data['route']['flyFrom'],
+        to_airport=data['route']['flyTo'],
+        from_date=data['route']['utc_departure'].split('T')[0] + ' UTC',
+        return_date=data["route"][1]["local_departure"].split("T")[0] + ' UTC'
+    )
