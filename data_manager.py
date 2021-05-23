e77215f6e99ae6a89ef170b3e216beed3e8aba74
Jonathan Adithya <jo.adithya391@gmail.com>
2021-05-23 09:55:27 +0700

    add sheety configuration and DataManager class

---
+++ data_manager.py
@@ -0,0 +1,21 @@
+from dotenv import load_dotenv
+import requests
+import os
+
+load_dotenv()
+USERNAME = os.environ.get('SHEETY_USERNAME')
+PROJECT_NAME = os.environ.get('SHEETY_PROJECT_NAME')
+SHEET_NAME = os.environ.get('SHEETY_SHEET_NAME')
+BEARER = os.environ.get('SHEETY_BEARER')
+
+sheety_url = f'https://api.sheety.co/{USERNAME}/{PROJECT_NAME}/{SHEET_NAME}'
+headers = {
+    'Authorization': f'Bearer {BEARER}',
+    'Content Type': 'application/json'
+}
+
+
+class DataManager:
+    def __init__(self):
+        self.sheet_data = {}
+
