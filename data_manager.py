a8f78285f30947f82fa7a4f524571e34d0259319
Jonathan Adithya <jo.adithya391@gmail.com>
2021-05-23 12:08:06 +0700

    add default value of lowest price to 0

--- data_manager.py
+++ data_manager.py
@@ -22,6 +22,9 @@ class DataManager:
     def get_sheet_data(self):
         response = requests.get(url=sheety_url, headers=headers)
         self.sheet_data = response.json()['prices']
+        for data in self.sheet_data:
+            if data.get('lowestPrice') is None or data.get('lowestPrice') == '':
+                data['lowestPrice'] = 0
         return self.sheet_data
 
     def update_code(self):
@@ -29,6 +32,15 @@ class DataManager:
             new_data = {
                 'price': {
                     'iataCode': city['iataCode']
+                }
+            }
+            requests.put(url=f'{sheety_url}/{city["id"]}', json=new_data, headers=headers)
+
+    def update_price(self):
+        for city in self.sheet_data:
+            new_data = {
+                'price': {
+                    'lowestPrice': city['lowestPrice']
                 }
             }
             requests.put(url=f'{sheety_url}/{city["id"]}', json=new_data, headers=headers)
