b610d01b68cd57a27108abb6a3dda0f1e66e5d4b
Jonathan Adithya <jo.adithya391@gmail.com>
2021-05-24 11:35:23 +0700

    add send_sms method to send SMS using Twilio

--- notification_manager.py
+++ notification_manager.py
@@ -13,3 +13,11 @@ my_password = os.environ.get('PASSWORD')
 class NotificationManager:
     def __init__(self):
         self.client = Client(account_sid, auth_token)
+
+    def send_sms(self, message_body):
+        self.client.messages.create(
+            body=message_body,
+            from_=os.environ.get('VIRTUAL_PHONE'),
+            to=os.environ.get('PHONE_NUMBER')
+        )
+
