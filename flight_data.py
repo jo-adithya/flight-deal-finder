0a23083cb9b29b3a7cc15f7752943f9e1316af0b
Jonathan Adithya <jo.adithya391@gmail.com>
2021-05-24 11:33:26 +0700

    add stop over and via_city attribute in FlightData class

--- flight_data.py
+++ flight_data.py
@@ -1,5 +1,6 @@
 class FlightData:
-    def __init__(self, price, airline, from_city, to_city, from_airport, to_airport, from_date, return_date):
+    def __init__(self, price, airline, from_city, to_city, from_airport, to_airport,
+                 from_date, return_date, stop_over=0, via_city=''):
         self.price = price
         self.airline = airline
         self.from_city = from_city
@@ -8,4 +9,6 @@ class FlightData:
         self.to_airport = to_airport
         self.from_date = from_date
         self.return_date = return_date
+        self.stop_over = stop_over
+        self.via_city = via_city
         print(f'{to_city}: Rp {price}')
