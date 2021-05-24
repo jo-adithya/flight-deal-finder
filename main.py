c098da48400e56171c4146c4348f8aeb3556ffab
Jonathan Adithya <jo.adithya391@gmail.com>
2021-05-24 11:36:43 +0700

    send emails if there are any cheap flight deals

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
+from notification_manager import NotificationManager
 from flight_search import get_code, check_flights
 from data_manager import DataManager
-from twilio.rest import Client
-from dotenv import load_dotenv
 import datetime as dt
-import os
 
-load_dotenv()
 ORIGIN_CITY = 'CGK'
 data_manager = DataManager()
+notification_manager = NotificationManager()
+
 sheet_data = data_manager.get_sheet_data()
 
 if sheet_data[0]['iataCode'] == '':
@@ -20,10 +19,6 @@ today = dt.datetime.now()
 tomorrow = (today + dt.timedelta(days=1)).strftime('%d/%m/%Y')
 six_months = (today + dt.timedelta(days=6 * 30)).strftime('%d/%m/%Y')
 
-account_sid = os.environ['TWILIO_ACCOUNT_SID']
-auth_token = os.environ['TWILIO_AUTH_TOKEN']
-client = Client(account_sid, auth_token)
-
 for destination in sheet_data:
     flight_data = check_flights(
         from_city=ORIGIN_CITY,
@@ -32,14 +27,27 @@ for destination in sheet_data:
         to_date=six_months
     )
 
-    if flight_data is not None and (flight_data.price < destination['lowestPrice'] or destination['lowestPrice'] == 0):
-        message = client.messages.create(
-            body=f"Low Price Alert!\n"
-                 f"Only Rp {flight_data.price} to fly from {flight_data.from_city}-{flight_data.from_airport} "
-                 f"to {flight_data.to_city}-{flight_data.to_airport}, "
-                 f"from {flight_data.from_date} to {flight_data.return_date}.",
-            from_=os.environ.get('VIRTUAL_PHONE'),
-            to=os.environ.get('PHONE_NUMBER')
-        )
+    if flight_data is None:
+        destination['lowestPrice'] = 'No Flights'
+        continue
+
+    if flight_data.price < destination['lowestPrice'] or destination['lowestPrice'] == 0:
+        users = data_manager.get_user_emails()
+        user_details = [(row['email'], ' '.join([row['firstName'], row['lastName']])) for row in users]
+
+        message = f'Low Price Alert!\n' \
+                  f'Only Rp {flight_data.price} to fly from {flight_data.from_city}-{flight_data.from_airport} ' \
+                  f'to {flight_data.to_city}-{flight_data.to_airport}, ' \
+                  f'from {flight_data.from_date} to {flight_data.return_date}.'
+
+        if flight_data.stop_over > 0:
+            message += f'\nFlight has {flight_data.stop_over} stop over, via {flight_data.via_city}'
+
+        notification_manager.send_sms(message)
+
+        link = f'https://www.google.co.uk/flights?hl=en#flt={flight_data.from_airport}.{flight_data.to_airport}.' \
+               f'{flight_data.from_date}*{flight_data.to_airport}.{flight_data.from_airport}.{flight_data.return_date}'
+        notification_manager.send_emails(user_details, message, link)
+
         destination['lowestPrice'] = flight_data.price
 data_manager.update_price()
